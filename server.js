04d285fc80fc3571c120bea7e519c6654a015112
Lionel Manje <manjetang@yahoo.com>
2018-09-07 00:56:12 -0700

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.port || 8080;
+var port = process.env.port || 3000;
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.use(bodyParser.urlencoded({extended: true}));
