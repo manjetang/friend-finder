d279f346e23a7d94a9ce80640aa52e2b86e7189e
Lionel Manje <manjetang@yahoo.com>
2018-09-08 13:25:46 -0700

    fixing case sensitivity

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var port = process.env.port || 3000;
 app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.text());
+// app.use(bodyParser.text());
 // app.use(express.static(__dirname + '/app/data/images'));
 
-require("./routing/apiroutes")(app);
-require("./routing/htmlroutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(port, function() {
     console.log("App listening on port: " + port);
