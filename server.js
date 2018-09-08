f7d8931ebed86980bff9036220832dccb05a16ac
Lionel Manje <manjetang@yahoo.com>
2018-09-08 13:39:06 -0700

    uppercassing port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
